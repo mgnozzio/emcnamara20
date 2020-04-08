35f5c4ed159180f45fa09360a81033799b1d5a58
emcnamara20 <63367439+emcnamara20@users.noreply.github.com>
2020-04-08 16:01:07 -0400

    adding files

---
+++ HelloWorld/src/HelloWorld2.java
@@ -0,0 +1,18 @@
+
+public class HelloWorld2 {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+		int num=5;
+		System.out.println(num);
+		double d=3.7;
+		System.out.println(num+d);
+		int i=2;
+		System.out.println(num/i);
+		for(int p=8;p<=24;p=p+3) {
+			System.out.print(p);
+		}
+
+	}
+
+}
